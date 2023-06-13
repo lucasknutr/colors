fa5fb078c6d372f1c9f64cd283bf8a916c4855c3
Lucas <lucasclaudinocc@gmail.com>
2023-06-13 00:06:50 -0300

    Added some cleaner code

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 let colorBoxes = document.querySelectorAll(".color-box");
 let rgbTextDisplay = document.getElementById("rgb");
+let r ;
+let g;
+let b;
+
+const randomizeColors = () => {
+    r = [];
+    g = [];
+    b = [];
+    for (i = 0; i < 9; i++){
+        r.push(Math.floor(Math.random() * 256))
+    }
+    for (i = 0; i < 9; i++){
+        g.push(Math.floor(Math.random() * 256))
+    }
+    for (i = 0; i < 9; i++){
+        b.push(Math.floor(Math.random() * 256))
+    }
+}
+
+randomizeColors();
 
 // Generates random RGB values \ Gera valores RGB aleatórios
 
-const generateColors = () => {
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-
+const generateColors = (r, g, b) => {
     return `rgb(${r}, ${g}, ${b})`;
 }
+
+
 
 
 // Inputs the random rgb values in each background of the circles \ coloca cor no background de cada um dos círculos
 
 const generateBackground = () => {
     for (i = 0; i < 9; i++) {
-        colorBoxes[i].style.background = generateColors();
+        colorBoxes[i].style.background = generateColors(r[i], g[i], b[i]);
     }
 }
 
@@ -33,12 +51,18 @@ generateBackground();
 let number = sortNumber();
 
 rgbTextDisplay.textContent = colorBoxes[number].style.background;
+let info = rgbTextDisplay.textContent;
 
 colorBoxes.forEach(element => {
     element.addEventListener("click", function () {
         if (element.style.background === colorBoxes[number].style.background) {
             alert("YOU WIN!");
+
+            randomizeColors();
+            
             generateBackground();
+            
+            
             sortNumber();
             rgbTextDisplay.textContent = colorBoxes[number].style.background;
         }
